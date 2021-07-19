ae162c0373cc22c106854dcb0e55ea7e01af94e3
Ilya Blokh <ilyablokh@gmail.com>
2021-07-19 18:14:16 +0500

    project init. import blocks

---
+++ Source/LevelGenerationDemo/LevelGenerationDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LevelGenerationDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEVELGENERATIONDEMO_API ALevelGenerationDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
